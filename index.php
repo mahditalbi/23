ed1b1c741af77bd8eca1396ad3c1dd53d83a9d7a
mahditalbi <mahdi.4afm@gmail.com>
2016-07-25 11:43:40 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
-define('API_KEY','XXXX');
-$the_admin_id = 0;
+define('API_KEY','191413689:AAEZadOGfo-V8WvEG4Pi7fBATKAUjz-5uK0');
+$the_admin_id = 175319648;
 
 file_put_contents("count",file_get_contents("count")+1);
 
